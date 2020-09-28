c1478b327f34d64a22561a8cbaeed369c997618f
Matthieu <matt@pilot.pm>
2020-09-28 16:16:12 +0200

    Prepare docker for test suite

---
+++ cmd/list_test.go
@@ -0,0 +1,11 @@
+package cmd
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	fmt.Println("I am running")
+	// db.Query()
+}
--- cmd/root_test.go
+++ cmd/root_test.go
@@ -2,12 +2,62 @@ package cmd
 
 import (
 	"bytes"
+	"database/sql"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 	"testing"
 
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
 	"github.com/spf13/cobra"
 )
+
+var db *sql.DB
+var pool *dockertest.Pool
+var resource *dockertest.Resource
+var database = "postgres"
+var err error
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func setup() {
+	pool, err = dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	resource, err = pool.Run("postgres", "9.6", []string{"POSTGRES_PASSWORD=secret", "POSTGRES_DB=" + database})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err = pool.Retry(func() error {
+		var err error
+		db, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", resource.GetPort("5432/tcp"), database))
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	fmt.Printf("\033[1;36m%s\033[0m", "> Setup completed\n")
+}
+
+func teardown() {
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+	fmt.Printf("\033[1;36m%s\033[0m", "> Teardown completed\n")
+}
 
 func NewRootCmd() *cobra.Command {
 	return rootCmd
